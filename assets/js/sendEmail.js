4f97a239a76aecaaf68b6c7736c33cee1797e514
marktodman <drmtodman@gmail.com>
2022-06-27 16:15:47 +0000

    feat: Add email service to form submission

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,13 @@
+function sendMail(contactForm) {
+    emailjs.send('service_wz11so9', 'template_615b2zl', {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+    return false;
+}
