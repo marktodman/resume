f5e10ef7ac00894b282781764b0345816f639040
marktodman <drmtodman@gmail.com>
2022-06-27 12:52:54 +0000

    fix: Coding errors and error message display on GitHub page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -57,15 +59,20 @@ function fetchGitHubInformation(event) {
         function (firstResponse, secondResponse) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
-            $("gh-user-data").html(userInformationHTML(userData));
-            $("gh-repo-data").html(repoInformationHTML(repoData));
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
-                $("gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
-                $("gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
+                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
